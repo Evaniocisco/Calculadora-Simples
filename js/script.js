9f17dce3d264b7584cf88ac84ec531a167117e81
Evaniocisco <evanio1915@gmail.com>
2022-03-16 15:01:04 +0100

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var nome = window.prompt('Qual é o seu nome')
+
+alert('SEJA BEM-VINDO, MBORA SOMAR MENINOS') 
+
+
+var n1 = Number.parseInt (window.prompt('Digite o primeiro número')) 
+var n2 = Number.parseInt (window.prompt('Digite o primeiro número'))
+var s = n1+n2  
+window.alert('O resultado da soma é ' + s)
