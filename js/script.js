cab0e06d460e110560f307370b242f72af6acb2e
Evaniocisco <evanio1915@gmail.com>
2022-03-17 14:12:51 +0100

    segundo commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,38 @@
 alert('SEJA BEM-VINDO, MBORA SOMAR MENINOS') 
 
 
-var n1 = Number.parseInt (window.prompt('Digite o primeiro número')) 
-var n2 = Number.parseInt (window.prompt('Digite o primeiro número'))
-var s = n1+n2  
-window.alert('O resultado da soma é ' + s)
+//var n1 = Number.parseInt (window.prompt('Digite o primeiro número')) 
+//var n2 = Number.parseInt (window.prompt('Digite o primeiro número'))
+//var s = n1+n2  
+//window.alert('O resultado da soma é ' + s) 
+
+let n1 = prompt("Digite o primeiro numero : ")
+
+let n2 = prompt("Digite o segundo numero : ")
+
+
+
+
+
+let operacao = prompt("Digita operaçao :")
+
+switch (operacao){
+        case '+':
+        alert(parseFloat(n1) + parseFloat(n2));
+        break;
+
+        case '-':
+        alert(parseFloat(n1) - parseFloat(n2));
+        break;
+
+        case '*':
+        alert(parseFloat(n1) * parseFloat(n2));
+        break;
+
+        case '/':
+        alert(parseFloat(n1) / parseFloat(n2));
+        break;
+
+        default:
+            alert("OPERAÇAO INCORRETA")
+}
